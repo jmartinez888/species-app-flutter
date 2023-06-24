4b7ffa1793803d0aecc7eb3fec46fda28b14a3dc
jdelaguila-dev <jdelaguila@devcloud.pe>
2023-06-24 00:47:30 -0500

    card v1

--- lib/src/presentation/views/home/pages/home_page.dart
+++ lib/src/presentation/views/home/pages/home_page.dart
@@ -1,21 +1,55 @@
-// Material
 import 'package:flutter/material.dart';
+import 'package:species/src/presentation/global/widgets/cards/species_card.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const List<Tab> tabs = [
-      Tab(text: 'Tab 1'),
-      Tab(text: 'Tab 2'),
-      Tab(text: 'Tab 3'),
-    ];
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
+      appBar: AppBar(
+        title: const Text('Species'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {},
+            ),
+          ],
         ),
-        body: const Center());
+      ),
+      body: MasonryGridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: 6,
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
+        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) => SpeciesCard(
+          titleCard: 'Pato Crestudo',
+          subtitleCard: 'Sarkidiornis sylvicola',
+          cardColor: Colors.red.withOpacity(0.1),
+          borderSideColor: Colors.red,
+          ratingStarscolor: Colors.red,
+          preferenciesButtonsColor: Colors.red,
+          iconColorCard: Colors.white,
+        ),
+      ),
+    );
   }
 }
