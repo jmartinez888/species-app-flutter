a86c88ae57a5ba3f67b3831a44543a9fce96ca54
jdelaguila-dev <jdelaguila@devcloud.pe>
2023-06-27 23:33:22 -0500

    fixing errors

--- lib/src/presentation/views/home/pages/home_page.dart
+++ lib/src/presentation/views/home/pages/home_page.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:species/src/presentation/global/colors/colors.dart';
-import 'package:species/src/presentation/global/widgets/cards/custom_card.dart';
-import 'package:species/src/presentation/global/widgets/custom_icon_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,67 +6,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: [
-          CustomIconButtom(
-            backgroundColor: CustomColors.bird,
-            iconColor: CustomColors.birdContainer,
-            icon: Icons.add,
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: MasonryGridView.builder(
-        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+        appBar: AppBar(
+          title: const Text('Home'),
         ),
-        itemCount: 6,
-        mainAxisSpacing: 8.0,
-        crossAxisSpacing: 8.0,
-        itemBuilder: (context, index) {
-          return CustomItemCard(
-            image: 'https://picsum.photos/300/400',
-            name: 'Nombre de la especie',
-            scientificName: 'Nombre científico',
-            backgroundCardColor: CustomColors.birdContainer,
-            borderCardColor: CustomColors.bird,
-            onTap: () {},
-            ratingStars: const [
-              Wrap(
-                spacing: 4.0,
-                children: [
-                  Icon(Icons.star_rounded,
-                      size: 16.0, color: CustomColors.bird),
-                  Icon(Icons.star_rounded,
-                      size: 16.0, color: CustomColors.bird),
-                  Icon(Icons.star_rounded,
-                      size: 16.0, color: CustomColors.bird),
-                  Icon(Icons.star_rounded,
-                      size: 16.0, color: CustomColors.bird),
-                  Icon(Icons.star_rounded,
-                      size: 16.0, color: CustomColors.bird),
-                ],
-              )
-            ],
-            actions: [
-              CustomIconButtom(
-                backgroundColor: CustomColors.bird,
-                iconColor: CustomColors.birdContainer,
-                icon: Icons.playlist_add,
-                onPressed: () {},
-              ),
-              const SizedBox(width: 8.0),
-              CustomIconButtom(
-                backgroundColor: CustomColors.bird,
-                iconColor: CustomColors.birdContainer,
-                icon: Icons.favorite_outline_outlined,
-                onPressed: () {},
-              )
-            ],
-          );
-        },
-      ),
-    );
+        body: const Center());
   }
 }
