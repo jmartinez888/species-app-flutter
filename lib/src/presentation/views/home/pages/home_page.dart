6303e72da0053b49fde784b87354e071f2033734
jdelaguila-dev <jdelaguila@devcloud.pe>
2023-06-29 13:16:10 -0500

    fix conflicts

--- lib/src/presentation/views/home/pages/home_page.dart
+++ lib/src/presentation/views/home/pages/home_page.dart
@@ -1,3 +1,4 @@
+// Material
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,9 +7,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home'),
-        ),
-        body: const Center());
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(),
+    );
   }
 }
