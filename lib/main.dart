96362d6d7b733523be362d9f6d22f1908cbd52f5
jdelaguila-dev <jdelaguila@devcloud.pe>
2023-06-29 13:23:36 -0500

    deleting dev packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:species/src/my_app.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) => const MyApp(),
-  ));
+  runApp(const MyApp());
 }
